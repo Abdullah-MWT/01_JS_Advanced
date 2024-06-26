37eb1c45f2644e3a846e7148104114d0df7a2fff
Abdullah-MWT <abdullahdeveloper2018@gmail.com>
2024-06-26 11:44:41 +0500

    My First JS Code Folder

---
+++ 09_DatesTime.js
@@ -0,0 +1,5 @@
+console.log("Hi");
+let todayDate = new Date();
+console.log(todayDate);
+console.log(new Date("2024-06-26").toString());
+console.log(new Date(2024,6,26,6,28,45,56).toDateString());
